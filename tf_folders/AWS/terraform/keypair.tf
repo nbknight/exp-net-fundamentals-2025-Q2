c212d8da9134a9ecf1a2c4f2a70dff638690dd2f
Nick <nicholas.b.knight@gmail.com>
2025-06-08 00:44:19 -0400

    Auto create the keypair

--- tf_folders/AWS/terraform/keypair.tf
+++ tf_folders/AWS/terraform/keypair.tf
@@ -7,3 +7,9 @@ resource "aws_key_pair" "bootcamp" {
   key_name   = "nwbootcampkey"                  # your chosen name
   public_key = tls_private_key.bootcamp.public_key_openssh
 }
+
+resource "local_file" "bootcamp_key_pem" {
+  content         = tls_private_key.bootcamp.private_key_pem
+  filename        = "${path.root}/nwbootcampkey.pem"
+  file_permission = "0400"
+}
